2046557ed887c7f6d230d5574f1b1abc9723fa21
Elisenda <elisenda.monserrat@gmail.com>
2021-03-07 11:59:24 +0100

    iteration 1 done

--- js/index.js
+++ js/index.js
@@ -1,72 +1,43 @@
 // ITERATION 1
-
 function updateSubtotal(product) {
-  
-  const price = product.querySelector('.price span');
-  const quantity = product.querySelector('.quantity input');
+  console.log('Calculating subtotal, yey!');
 
-  // Calculation of the Subtotal of each product
-  const subtotalPrice = price.innerText * quantity.value;
+  // The following varibles are string type => "25$"
+  const price = document.querySelector(".price span");
+  const quantity = document.querySelector(".quantity input");
 
-  // Quering the subtotal element where we want to update the subtotal $
-  const subtotalResult = product.querySelector(".subtotal span");
+  const priceValue = price.innerHTML;
+  const quantityValue = quantity.value;
 
-  // Adding the subtotal price to the subtotal element
-  subtotalResult.innerText = subtotalPrice;
 
-  return subtotalPrice;
+  const totalCount = priceValue * quantityValue;
+
+  const subtotalValue = document.querySelector(".subtotal span").innerText = totalCount;
+
 }
-
 function calculateAll() {
-
-  // ITERATION 2 / 3
-  // Querying and selecting all product rows (t.product) in the table
-  const cartProducts = document.querySelectorAll("tr.product");
-
-  // Converting the NodeList into an array so we can use methods as forEach, filter..
-  const cartProductsArr = Array.from(cartProducts);
-
-  // Using the forEach method to iterate on each product to get their values and callback the updateSubtotal
-  let sumAll = 0;
-  cartProductsArr.forEach(element => {
-    const sumAll += updateSubtotal(element);
-  });
-
-  // Querying the element we want to upadte (Total) and passing it the sumAll result
-  const total = document.querySelector("#total-value > span");
-  total.innerText = sumAll;
-  return sumAll;
+  // code in the following two lines is added just for testing purposes.
+  // it runs when only iteration 1 is completed. at later point, it can be removed.
+  const singleProduct = document.querySelector('.product');
+  updateSubtotal(singleProduct);
+  // end of test
+  // ITERATION 2
+  //... your code goes here
+  // ITERATION 3
+  //... your code goes here
 }
-
 // ITERATION 4
-
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-
-  //Querying the parent and the child elements where the event fires
-  const cartTable = document.querySelector("#cart > tbody")
-  const productRow = target.parentNode.parentNode;
-
-  // Removing the child where the event fires
-  cartTable.removeChild(productRow);
+  //... your code goes here
 }
-
 // ITERATION 5
-
 function createProduct() {
   //... your code goes here
 }
-
 window.addEventListener('load', () => {
-  // Calculate Prices
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //Remove | 1. Querying the document for all "Remove" buttons
-  const removeBtn = document.querySelectorAll(".btn-remove");
-  let removeBtnArr = Array.from(removeBtn);
-
-  // Remove | 2. Looping through each button and adding an EventListener
-  removeBtnArr.forEach(btn => btn.addEventListener("click", removeProduct));
+  //... your code goes here
 });
